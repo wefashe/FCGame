01a7054b357344a699005e49ea9a28202931d24a
wefashe <wefashe@qq.com>
2024-12-13 14:22:31 +0800

    页面更新

---
+++ docs/precache-manifest.7f2f71d01c2d011832a54ac937512b7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6709c4fe61bd8f1592d540f1ebcf08cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4759abeb618b61269549",
+    "url": "/static/css/main.6a0f1550.chunk.css"
+  },
+  {
+    "revision": "ef8ba064ca5c3b6f5818",
+    "url": "/static/js/2.643db120.chunk.js"
+  },
+  {
+    "revision": "ce788a1fa508dab3627efbe6528a9a6f",
+    "url": "/static/js/2.643db120.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4759abeb618b61269549",
+    "url": "/static/js/main.e0df947c.chunk.js"
+  },
+  {
+    "revision": "dc6c9145439f3ba64f0c",
+    "url": "/static/js/runtime-main.be5a99a7.js"
+  }
+]);
